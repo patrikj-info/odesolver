9289a66631ad9f3af40e4d6e0d290cd9d854e673
Patrik-J <patrik_j@gmx.de>
2025-11-14 19:42:01 +0100

    Test added

--- tests/test_odesolver.py
+++ tests/test_odesolver.py
@@ -1,8 +1,11 @@
 import pytest 
-from odesolver import ODESolver 
+from odesolver.ode import ODE 
+from odesolver.odesolver import ODESolver
 
 def test():
-    return "Test"
+    ode = ODE.readHomogenousODE("5.0x'' + 1.0x' + 3.0x")
+    res = ODESolver.solveODE(ode=ode, method=ODESolver.EULER, initial_conditions=[1.0, 0.0], t0=0, t_final=50, h=0.01)
+    ODESolver.plotSolution(res)
 
 if __name__ == "__main__":
     pytest.main()
