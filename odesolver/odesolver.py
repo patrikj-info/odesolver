7a871f31f7bfd1d4c240ab5923c3d8d30c792d4f
patrikj-info <patrik.jelic@uni-a.de>
2025-11-16 16:24:41 +0100

    FPS default changed

--- odesolver/odesolver.py
+++ odesolver/odesolver.py
@@ -52,5 +52,5 @@ class ODESolver:
                 data : np.ndarray
 
         """
-        Plotter.plot(data=data, animated=kwargs.get("animated", False), phase_space=kwargs.get("phase_space", False), save=kwargs.get("save", False), filename=kwargs.get("filename", ""), xlabel=kwargs.get("xlabel", ""), ylabel=kwargs.get("ylabel", ""), title=kwargs.get("title", ""), fps=kwargs.get("fps", 60), fileformat=kwargs.get("fileformat", ""))
+        Plotter.plot(data=data, animated=kwargs.get("animated", False), phase_space=kwargs.get("phase_space", False), save=kwargs.get("save", False), filename=kwargs.get("filename", ""), xlabel=kwargs.get("xlabel", ""), ylabel=kwargs.get("ylabel", ""), title=kwargs.get("title", ""), fps=kwargs.get("fps", 400), fileformat=kwargs.get("fileformat", ""))
         
