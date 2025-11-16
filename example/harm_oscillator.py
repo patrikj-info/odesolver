cc9b0444b1f77da1e24c0b3327baaefb021376c7
patrikj-info <patrik.jelic@uni-a.de>
2025-11-16 16:37:49 +0100

    Example file added

--- example/harm_oscillator.py
+++ example/harm_oscillator.py
@@ -8,10 +8,10 @@ ode = ODE.readHomogenousODE("4.0x'' + 0.1x' + 2.0x")
 sol = ODESolver.solveODE(ode=ode, method="euler", initial_conditions=[1.0, 0.0], t0=0, t_final=10, h=0.01)
 
 # plot solution
-ODESolver.plotSolution(sol, phase_space=True, save=True, filename="harm_oscillator_euler.png", fileformat="png", fps=500)
+ODESolver.plotSolution(sol, phase_space=True, save=True, filename="harm_oscillator_euler.png", fileformat="png")
 
 # find solution using Runge-Kutta 4
 sol = ODESolver.solveODE(ode=ode, method="rk4", initial_conditions=[1.0, 0.0], t0=0, t_final=10, h=0.01)
 
 # plot solution
-ODESolver.plotSolution(sol, phase_space=True, save=True, filename="harm_oscillator_rk4.png", fileformat="gif", fps=500)
+ODESolver.plotSolution(sol, phase_space=True, save=True, filename="harm_oscillator_rk4.png", fileformat="gif")
